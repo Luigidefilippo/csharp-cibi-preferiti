2addc9fc2bf86394d47be3ddf2a5a3462e2a8127
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-05 15:43:29 +0200

    Print list of favorite foods

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -2,4 +2,13 @@
 
 string[] FavoriteFood = { "Pizza", "Cheesborger", "Pasta", "Ice-Creme", "Sushi" };
 
+Console.WriteLine(FavoriteFood.Length);
+
+Console.WriteLine("This is my ranking of my favorite foods:");
+
+for (int i = 0; i < FavoriteFood.Length; i++)
+{
+    Console.WriteLine(FavoriteFood[i]);
+}
+
 
